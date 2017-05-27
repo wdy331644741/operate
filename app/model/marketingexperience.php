36080676b79dc56a1bf2ff519e3119e666622670
IzhuangbeiWdy <wdy@izhuanbei.com>
2017-05-27 21:31:50 +0800

    。

--- app/model/marketingexperience.php
+++ app/model/marketingexperience.php
@@ -17,7 +17,7 @@ class MarketingExperience extends Model {
         $start_time = $time.' 00:00:00';
         $end_time = $time.' 23:59:59';
         return $this->fields('id,user_id',false)
-            ->where("`create_time` >= '{$start_time}' and `create_time` <= '{$end_time}' and `is_activate` = '0' and `is_use` = 0")
+            ->where("`create_time` >= '{$start_time}' and `create_time` <= '{$end_time}' and `is_activate` = '0' and `is_use` = 1")
             ->get()->resultArr();
     }
 
