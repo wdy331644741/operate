0317703b4a307fd85527f91825e037ec04c28193
IzhuangbeiWdy <wdy@izhuanbei.com>
2017-07-03 18:33:40 +0800

    主页显示过期时间

--- app/admin/index.php
+++ app/admin/index.php
@@ -10,6 +10,49 @@ function index(){
    // dump($where);die;
     $userDayNums=$userModel->where($where)->countNums();
     //dump($data);die;
+
+    //$awardTable = ['award_interestcoupon','award_experience','config_earnings','marketing_activity'];
+    //$remindDays = 7;
+	$remindInfo = array(
+		'interestcoupon' =>array(
+			'show'       =>'id,title',
+			'table'      => 'award_interestcoupon',
+			'desc'       => '加息券',
+			'filed'      => 'effective_end',
+			'remindDays' => '7',
+		),
+		'experience'     =>array(
+			'show'       =>'id,title',
+			'table'      => 'award_experience',
+			'desc'       => '体验金',
+			'filed'      => 'effective_end',
+			'remindDays' => '7',
+		),
+		'earnings'       =>array(
+			'show'       =>'id,title',
+			'table'      => 'config_earnings',
+			'desc'       => '邀请收益、',
+			'filed'      => 'end_time',
+			'remindDays' => '7',
+		),
+		'activity'       =>array(
+			'show'       =>'id,title',
+			'table'      => 'marketing_activity',
+			'desc'       => '活动',
+			'filed'      => 'end_time',
+			'remindDays' => '7',
+		),
+	);
+
+	$tipArray = [];
+	foreach ($remindInfo as $key => $value) {
+		# code...
+		$sql = "SELECT {$value['show']},DATEDIFF({$value['filed']},curdate()) AS remind from `{$value['table']}` WHERE DATEDIFF({$value['filed']},curdate())<{$value['remindDays']} AND DATEDIFF({$value['filed']},curdate())>0 AND `is_del` = 0 ORDER BY {$value['filed']} DESC";
+		// echo $sql;
+		$tipArray[$key] = $userModel->query($sql)->resultArr();
+	}
+	// var_export($tipArray);
+	$framework->smarty->assign('tipArray',$tipArray);
     $framework->smarty->assign('userNum',$userDayNums);
     $framework->smarty->display('index/index.html');
 }
