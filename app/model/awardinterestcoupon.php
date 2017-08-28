21a52a82ce22510b7d734b9c5d77da6ea866f5a9
IzhuangbeiWdy <wdy@izhuanbei.com>
2017-08-28 22:25:44 +0800

    merge ladder

--- app/model/awardinterestcoupon.php
+++ app/model/awardinterestcoupon.php
@@ -11,21 +11,21 @@ class AwardInterestcoupon extends Model
             $this->initArData($pkVal);
     }
 
-    public function filterUsefulInterestCoupon($nodeId) 
-    {
-        $nowTime = date("Y-m-d H:i:s");
-        if(is_array($nodeId)){
-            $nodeIdStr = implode(',', $nodeId);
-            return $this->where("`limit_node` in ({$nodeIdStr}) and `effective_end` > '{$nowTime}' and status = 1 and is_del = 0")
-                ->orderby("id DESC")
-                ->get()->resultArr();
-        }else{
-            return $this->where("`limit_node` = {$nodeId} and `effective_end` > '{$nowTime}' and status = 1 and is_del = 0")
-                ->orderby("id DESC")
-                ->get()->rowArr();
-        }
+    public function filterUsefulInterestCoupon($nodeId)
+    { 
+        $nowTime = date("Y-m-d H:i:s"); 
+        if(is_array($nodeId)){ 
+            $nodeIdStr = implode(',', $nodeId); 
+            return $this->where("`limit_node` in ({$nodeIdStr}) and `effective_end` > '{$nowTime}' and status = 1 and is_del = 0") 
+                ->orderby("id DESC") 
+                ->get()->resultArr(); 
+        }else{ 
+            return $this->where("`limit_node` = {$nodeId} and `effective_end` > '{$nowTime}' and status = 1 and is_del = 0") 
+                ->orderby("id DESC") 
+                ->get()->rowArr(); 
+        } 
         
-    }
+    } 
 
     public function switchStausById($id)
     {
