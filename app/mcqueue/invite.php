8e1d1c18cb78d48dd8767b11f6ac8ee4ce2f92cc
IzhuangbeiWdy <wdy@izhuanbei.com>
2017-09-14 16:41:48 +0800

    再次还原 每隔5个发送加息券 发送判断

--- app/mcqueue/invite.php
+++ app/mcqueue/invite.php
@@ -114,7 +114,8 @@ function invitecoupon(){
         
     // }
     
-
+    /**
+    //9.14再次修改  不要判断了。前提是用户中心推过来的消息，一个都不漏
     if($invites % $eachSend == 0){
         //发一张2%加息券  直接发放没有什么逻辑，直接调用手动发放奖品rpc
         // $giveInterestcouponModel = new \Model\MarketingInterestcoupon();
@@ -131,6 +132,18 @@ function invitecoupon(){
             logs($fromUserId.":补发加息券","bufajiaxiquan");
             echo $fromUserId.":补发加息券";
         }
+    }
+    **/
+
+    //9.14再次修改  不要判断了。前提是用户中心推过来的消息，一个都不漏
+    if($invites % $eachSend == 0){
+        //发一张2%加息券  直接发放没有什么逻辑，直接调用手动发放奖品rpc
+        // $giveInterestcouponModel = new \Model\MarketingInterestcoupon();
+        // $giveInterestcouponModel->giveUserInterest();
+        // exit("55555");
+        $send = new SendCouponRpcImpl();
+        $sendRes = $send->activitySendAction(1, $fromUserId, $nodeId);
+        exit("发送加息券成功");
     }
 
 }
