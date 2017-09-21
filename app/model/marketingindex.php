4e55eb0f6507dbc25d5dd3bb7fb945820c3510bb
IzhuangbeiWdy <wdy@izhuanbei.com>
2017-09-21 10:08:11 +0800

    list 启动、禁用

--- app/model/marketingindex.php
+++ app/model/marketingindex.php
@@ -2,6 +2,8 @@
 namespace Model;
 class MarketingIndex extends Model
 {
+    const STATUS_TRUE = 1;
+    const STATUS_FALSE = 0;
     public function __construct($pkVal = '')
     {
         parent::__construct('marketing_index');
@@ -9,6 +11,17 @@ class MarketingIndex extends Model
             $this->initArData($pkVal);
     }
 
+    public function switchStausById($id)
+    {
+        $where = ['id' => $id];
+        $row = $this->where($where)->get()->row();
+        if ($row->status == self::STATUS_TRUE)
+            $status = self::STATUS_FALSE;
+        else
+            $status = self::STATUS_TRUE;
+
+        return $this->where($where)->upd(['status' => $status]);
+    }
 
     public function getDefaultSlogen(){
     	return $this->where(['status'=>1,'pos'=>1,'is_del'=>0])->get()->rowArr();
