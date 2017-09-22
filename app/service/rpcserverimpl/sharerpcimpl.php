7a8ee2abc83a6fa31e560300811fce66a7bef5df
daiqing <daiqing@wanglibank.com>
2017-09-22 14:25:21 +0800

    修正微信分享参数url，为调用接口的地址

--- app/service/rpcserverimpl/sharerpcimpl.php
+++ app/service/rpcserverimpl/sharerpcimpl.php
@@ -31,13 +31,13 @@ class ShareRpcImpl extends BaseRpcImpl
      *
      * @JsonRpcMethod
      */
-    public function getSignPackage() {
+    public function getSignPackage($params) {
         $jsapiTicket = $this->getJsApiTicket();
 
         // 注意 URL 一定要动态获取，不能 hardcode.
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
+       // $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+        //$url = "$protocol$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+        $url = $params->url;
         $timestamp = time();
         $nonceStr = $this->createNonceStr();
 
